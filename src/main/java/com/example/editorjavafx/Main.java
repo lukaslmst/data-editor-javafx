17e688bb315a57034f5efd575a0ed2fa976a9b42
lukaslmst <luki.lumm@icloud.com>
2022-02-28 11:14:20 +0100

    update gitignore and added connection in Main

--- src/main/java/com/example/editorjavafx/Main.java
+++ src/main/java/com/example/editorjavafx/Main.java
@@ -6,18 +6,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+        @Override
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
 
-    public static void main(String[] args) {
-        launch();
-    }
+        public static void main(String[] args) throws SQLException {
+            String dbstring = "jdbc:oracle:thin:delphi.htl-leonding.ac.at:1521:delphidb";
+            Connection conn = DriverManager.getConnection(
+                    dbstring,"it190225",""
+            );
+            launch();
+        }
 }
