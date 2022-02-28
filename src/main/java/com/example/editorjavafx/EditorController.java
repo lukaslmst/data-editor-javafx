802a0a48fd855268a0e72a47bc51b7e63d7dfb16
lukaslmst <luki.lumm@icloud.com>
2022-02-28 11:01:44 +0100

    intial commit

---
+++ src/main/java/com/example/editorjavafx/EditorController.java
@@ -0,0 +1,14 @@
+package com.example.editorjavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class EditorController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
