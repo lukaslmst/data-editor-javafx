9c1901f6db7d32d970c7a5814db6a00e59476595
lukaslmst <luki.lumm@icloud.com>
2022-03-13 14:56:23 +0100

    connect Method in Controller

--- src/main/java/com/example/editorjavafx/EditorController.java
+++ src/main/java/com/example/editorjavafx/EditorController.java
@@ -1,14 +1,47 @@
 package com.example.editorjavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class EditorController {
-    @FXML
-    private Label welcomeText;
+
+
+    public static void show(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button connectBtn;
+
+    public void connect(ActionEvent event) throws SQLException {
+
+        String password;
+        String username;
+        String dbString;
+
+        String dbstring = "jdbc:oracle:thin:@delphi.htl-leonding.ac.at:1521:delphidb";
+
+        Connection conn = DriverManager.getConnection(
+                dbstring,"it190225","oracle"
+        );
+
+
     }
+
 }
--- src/main/java/com/example/editorjavafx/Main.java
+++ src/main/java/com/example/editorjavafx/Main.java
@@ -13,18 +13,10 @@ import java.sql.SQLException;
 public class Main extends Application {
         @Override
         public void start(Stage stage) throws IOException {
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-            stage.setTitle("Hello!");
-            stage.setScene(scene);
-            stage.show();
+            EditorController.show(stage);
         }
 
-        public static void main(String[] args) throws SQLException {
-            String dbstring = "jdbc:oracle:thin:@delphi.htl-leonding.ac.at:1521:delphidb";
-            Connection conn = DriverManager.getConnection(
-                    dbstring,"it190225","oracle"
-            );
+        public static void main(String[] args) {
             launch();
         }
 }
